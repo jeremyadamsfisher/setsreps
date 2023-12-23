ce60cb1ccb01f95d02bc6c80da42bdd9714fa472
Jeremy Fisher <jeremy.adams.fisher@gmail.com>
2023-12-23 10:12:18 -0800

    Add a user loading state

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 	import { userStore } from 'sveltefire';
 	import Header from './Header.svelte';
 	import LogIn from '$lib/components/log-in/log-in.svelte';
+	import {Circle} from "svelte-loading-spinners";
 
 	const user = userStore(auth);
 </script>
@@ -14,6 +15,10 @@
 	<main class="p-3 pt-5 sm:p-5 h-full overflow-auto">
 		{#if $user}
 			<slot />
+		{:else if $user === null}
+			<div class="h-full flex justify-center items-center">
+				<Circle color="white" size={30}  />
+			</div>
 		{:else}
 			<LogIn />
 		{/if}
