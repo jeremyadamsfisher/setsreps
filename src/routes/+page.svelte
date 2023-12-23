d9b2127a0e912f1f4106f69485c1a0575465a2e3
Jeremy Fisher <jeremy.adams.fisher@gmail.com>
2023-12-23 10:12:29 -0800

    Add a welcome screen

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Button from "$lib/components/ui/button/button.svelte";
+</script>
+
+<div class="flex h-full gap-2 items-center justify-center">
+    <a href="/workout">
+        <Button>Start a workout</Button>
+    </a>
+    <a href="/history">
+        <Button variant="secondary">Use an old workout as a template</Button>
+    </a>
+</div>
